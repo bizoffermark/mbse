b38cbc99a8d8a4c3693541ee3a0ab2583723f9e6
sukhijab <sukhijab@student.ethz.ch>
2022-11-29 00:04:09 +0100

    added cross entropy optimizer

--- mbse/optimizers/cross_entropy_optimizer.py
+++ mbse/optimizers/cross_entropy_optimizer.py
@@ -13,8 +13,9 @@ class CrossEntropyOptimizer(object):
             action_dim,
             num_samples=500,
             num_elites=50,
-            num_steps=100,
+            num_steps=200,
             seed=0,
+            init_var=5,
     ):
 
         self.func = func
@@ -22,22 +23,22 @@ class CrossEntropyOptimizer(object):
         self.num_elites = num_elites
         self.num_steps = num_steps
         self.action_dim = action_dim
-        self.mean = jnp.zeros(action_dim)
-        self.std = jnp.ones(action_dim)
         self.seed = seed
+        self.init_var = init_var
+        # self.exploration_std = 0.1
 
     @partial(jit, static_argnums=0)
-    def step(self):
+    def step(self, mean, std):
         key = jax.random.PRNGKey(self.seed)
-        samples = self.mean + jax.random.multivariate_normal(
+        samples = mean + jax.random.multivariate_normal(
             key=key,
-            mean=self.mean,
-            cov=jnp.diag(jnp.square(self.std)),
-            shape=(self.num_samples, ))
+            mean=jnp.zeros_like(mean),
+            cov=jnp.diag(jnp.ones_like(mean)),
+            shape=(self.num_samples, ))*std
 
-        values = vmap(self.func, 0, 0)(samples)
+        values = vmap(self.func)(samples)
 
-        best_elite_idx = np.argsort(-values)[:self.num_elites]
+        best_elite_idx = np.argsort(values, axis=0).squeeze()[-self.num_elites:]
 
         elites = samples[best_elite_idx]
         elite_values = values[best_elite_idx]
@@ -45,21 +46,17 @@ class CrossEntropyOptimizer(object):
 
     def optimize(self):
         best_value = -jnp.inf
-        best_sequence = self.mean
+        mean = jnp.zeros(self.action_dim)
+        std = jnp.ones(self.action_dim)*self.init_var
+        best_sequence = mean
         for i in range(self.num_steps):
-            elites, elite_values = self.step()
-            self.mean = jnp.mean(elites, axis=0)
-            self.std = jnp.std(elites, axis=0)
+            elites, elite_values = self.step(mean, std)
+            mean = jnp.mean(elites, axis=0)
+            std = jnp.std(elites, axis=0)
             best_elite = elite_values[0]
             if best_value <= best_elite:
                 best_value = best_elite
                 best_sequence = elites[0]
-
         return best_sequence
-
-    def reset(self):
-        self.mean = jnp.zeros(self.action_dim)
-        self.std = jnp.ones(self.action_dim)
-
 
 

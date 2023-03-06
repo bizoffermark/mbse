ab99e3bffaee070c2f5ad40ae067bb3440fd45d4
sukhijab <sukhijab@student.ethz.ch>
2023-03-06 17:32:38 +0100

    implemented calibration for BNNs

--- mbse/utils/models.py
+++ mbse/utils/models.py
@@ -6,6 +6,7 @@ from mbse.utils.network_utils import MLP
 import jax
 from mbse.utils.utils import gaussian_log_likelihood, rbf_kernel
 import optax
+from jax.scipy.stats import norm
 
 EPS = 1e-6
 
@@ -38,7 +39,7 @@ class ProbabilisticEnsembleModel(object):
         self.output_dim = output_dim
         self.mlp = MLP(
             features=features,
-            output_dim=2*output_dim,
+            output_dim=2 * output_dim,
             non_linearity=non_linearity
         )
         self.num_ensembles = num_ensemble
@@ -46,7 +47,7 @@ class ProbabilisticEnsembleModel(object):
         init = vmap(self.mlp.init, (0, None))
         self.net = self.mlp.apply
         self.rng = jax.random.PRNGKey(seed)
-        seed_sequence = jax.random.split(self.rng, self.num_ensembles+1)
+        seed_sequence = jax.random.split(self.rng, self.num_ensembles + 1)
         self.rng = seed_sequence[0]
         seed_sequence = seed_sequence[1:]
         particles = init(seed_sequence, example_input)
@@ -68,7 +69,8 @@ class ProbabilisticEnsembleModel(object):
                                 sig_max=self.sig_max,
                                 sig_min=self.sig_min,
                             )
-                        )
+                            )
+        self.num_ps = 10
         if initialize_train_fn:
             self._train_step = jit(lambda params, opt_state, x, y: self._train_fn(
                 predict_fn=self._predict,
@@ -79,6 +81,43 @@ class ProbabilisticEnsembleModel(object):
                 y=y,
             ))
 
+            def calculate_calibration_score(params, xs, ys, ps, alpha):
+                return self._calculate_calibration_score(
+                    predict_fn=self._predict,
+                    params=params,
+                    xs=xs,
+                    ys=ys,
+                    ps=ps,
+                    alpha=alpha,
+                    output_dim=self.output_dim,
+                )
+
+            self.calculate_calibration_score_fn = calculate_calibration_score
+
+            def calibration_errors(params, xs, ys, ps, alpha):
+                return self._calibration_errors(
+                    calibration_score_fn=self.calculate_calibration_score_fn,
+                    params=params,
+                    xs=xs,
+                    ys=ys,
+                    ps=ps,
+                    alpha=alpha,
+                    output_dim=self.output_dim,
+                )
+
+            self.calibration_errors = calibration_errors
+
+            def calculate_calibration_alpha(params, xs, ys):
+                return self._calculate_calibration_alpha(
+                    calibration_error_fn=self.calibration_errors,
+                    params=params,
+                    xs=xs,
+                    ys=ys,
+                    num_ps=self.num_ps,
+                    output_dim=self.output_dim,
+                )
+
+            self.calculate_calibration_alpha = jax.jit(calculate_calibration_alpha)
 
     @property
     def params(self):
@@ -86,7 +125,6 @@ class ProbabilisticEnsembleModel(object):
 
     def predict(self, x):
         return self._predict(self.particles, x)
-
 
     @staticmethod
     def _train_fn(predict_fn, update_fn, params, opt_state, x, y, prior_particles=None):
@@ -97,6 +135,7 @@ class ProbabilisticEnsembleModel(object):
             mu, sig = jnp.split(predictions, 2, axis=-1)
             logl = likelihood(y, mu, sig)
             return -logl.mean()
+
         # vmap over ensemble
         loss, grads = value_and_grad(likelihood_loss)(params)
         updates, new_opt_state = update_fn(grads,
@@ -117,6 +156,46 @@ class ProbabilisticEnsembleModel(object):
         self.opt_state = new_opt_state
         return loss, grad_norm
 
+    @staticmethod
+    def _calculate_calibration_alpha(calibration_error_fn, params, xs, ys, num_ps, output_dim) -> jax.Array:
+        # We flip so that we rather take more uncertainty model than less
+        ps = jnp.linspace(0, 1, num_ps + 1)[1:]
+        test_alpha = jnp.flip(jnp.linspace(0, 10, 100)[1:])
+        test_alphas = jnp.repeat(test_alpha[..., jnp.newaxis], repeats=output_dim, axis=1)
+        errors = vmap(calibration_error_fn, in_axes=(None, None, None, None, 0))(
+            params, xs, ys, ps, test_alphas)
+        indices = jnp.argmin(errors, axis=0)
+        best_alpha = test_alpha[indices]
+        assert best_alpha.shape == (output_dim,)
+        return best_alpha, errors[indices]
+
+    @staticmethod
+    def _calibration_errors(calibration_score_fn, params, xs, ys, ps, alpha, output_dim) -> jax.Array:
+        ps_hat = calibration_score_fn(params, xs, ys, ps, alpha)
+        ps = jnp.repeat(ps[..., jnp.newaxis], repeats=output_dim, axis=1)
+        return jnp.mean((ps - ps_hat) ** 2, axis=0)
+
+    @staticmethod
+    def _calculate_calibration_score(predict_fn, params, xs, ys, ps, alpha, output_dim):
+        assert alpha.shape == (output_dim,)
+
+        def calculate_score(x, y):
+            predictions = predict_fn(params, x)
+            mean, std = jnp.split(predictions, 2, axis=-1)
+            mu = jnp.mean(mean, axis=0)
+            eps_std = jnp.std(mean, axis=0)
+            al_uncertainty = jnp.sqrt(jnp.mean(jnp.square(std), axis=0))
+            cdfs = vmap(norm.cdf)(y, mu, eps_std * alpha + al_uncertainty)
+
+            def check_cdf(cdf):
+                assert cdf.shape == ()
+                return cdf <= ps
+
+            return vmap(check_cdf, out_axes=1)(cdfs)
+
+        cdfs = vmap(calculate_score)(xs, ys)
+        return jnp.mean(cdfs, axis=0)
+
 
 class FSVGDEnsemble(ProbabilisticEnsembleModel):
     def __init__(self,
@@ -128,7 +207,7 @@ class FSVGDEnsemble(ProbabilisticEnsembleModel):
         super(FSVGDEnsemble, self).__init__(*args, **kwargs, initialize_train_fn=False)
         n_prior_particles = n_prior_particles or self.num_ensembles
         init = vmap(self.mlp.init, (0, None))
-        seed_sequence = jax.random.split(self.rng, n_prior_particles+1)
+        seed_sequence = jax.random.split(self.rng, n_prior_particles + 1)
         self.rng = seed_sequence[0]
         seed_sequence = seed_sequence[1:]
         self.priors = init(seed_sequence, self.example_input)
@@ -155,8 +234,8 @@ class FSVGDEnsemble(ProbabilisticEnsembleModel):
                                       prior_bandwidth=self.prior_bandwidth,
                                       k_bandwidth=self.k_bandwidth,
                                       )
-            self._train_step = jit(train_step)
 
+            self._train_step = jit(train_step)
 
     def _prior(self, prior_particles, x):
         predictions = self._predict(prior_particles, x)
@@ -183,8 +262,8 @@ class FSVGDEnsemble(ProbabilisticEnsembleModel):
             k_bandwidth
     ):
         # mean_prior, k_prior = self._prior(prior_particles, x)
-        rbf = lambda z,v: rbf_kernel(z, v, bandwidth=prior_bandwidth)
-        kernel = lambda x: rbf(x, x) #K(x, x)
+        rbf = lambda z, v: rbf_kernel(z, v, bandwidth=prior_bandwidth)
+        kernel = lambda x: rbf(x, x)  # K(x, x)
         k_prior = kernel(x)
         k_prior = jnp.stack([k_prior, k_prior], axis=-1)
 
@@ -204,7 +283,7 @@ class FSVGDEnsemble(ProbabilisticEnsembleModel):
             likelihood = lambda x, cov_x: \
                 jax.scipy.stats.multivariate_normal.logpdf(x,
                                                            mean=jnp.zeros(x.shape[0]),
-                                                           cov=cov_x + 1e-4*jnp.eye(x.shape[0]))
+                                                           cov=cov_x + 1e-4 * jnp.eye(x.shape[0]))
             likelihood = jax.vmap(likelihood, in_axes=-1, out_axes=-1)
 
             def neg_log_prior(predictions):
@@ -212,7 +291,7 @@ class FSVGDEnsemble(ProbabilisticEnsembleModel):
                 log_sigma = jnp.log(std_pred + EPS)
                 altered_predictions = jnp.stack([mean_pred, log_sigma], axis=-1)
                 log_prior = likelihood(altered_predictions, k_prior)
-                return -log_prior.mean()/mean_pred.shape[-2]
+                return -log_prior.mean() / mean_pred.shape[-2]
 
             def neg_total_likelihood(predictions):
                 log_post = neg_log_post(predictions)
@@ -254,7 +333,6 @@ class KDEfWGDEnsemble(FSVGDEnsemble):
                                               *args,
                                               **kwargs)
 
-
     @staticmethod
     def _train_fn(
             predict_fn,
@@ -269,8 +347,8 @@ class KDEfWGDEnsemble(FSVGDEnsemble):
             k_bandwidth
     ):
         # mean_prior, k_prior = self._prior(prior_particles, x)
-        rbf = lambda z,v: rbf_kernel(z, v, bandwidth=prior_bandwidth)
-        kernel = lambda x: rbf(x, x) #K(x, x)
+        rbf = lambda z, v: rbf_kernel(z, v, bandwidth=prior_bandwidth)
+        kernel = lambda x: rbf(x, x)  # K(x, x)
         k_prior = kernel(x)
         k_prior = jnp.stack([k_prior, k_prior], axis=-1)
 
@@ -290,7 +368,7 @@ class KDEfWGDEnsemble(FSVGDEnsemble):
             likelihood = lambda x, cov_x: \
                 jax.scipy.stats.multivariate_normal.logpdf(x,
                                                            mean=jnp.zeros(x.shape[0]),
-                                                           cov=cov_x + 1e-4*jnp.eye(x.shape[0]))
+                                                           cov=cov_x + 1e-4 * jnp.eye(x.shape[0]))
             likelihood = jax.vmap(likelihood, in_axes=-1, out_axes=-1)
 
             def neg_log_prior(predictions):
@@ -298,7 +376,7 @@ class KDEfWGDEnsemble(FSVGDEnsemble):
                 log_sigma = jnp.log(std_pred + EPS)
                 altered_predictions = jnp.stack([mean_pred, log_sigma], axis=-1)
                 log_prior = likelihood(altered_predictions, k_prior)
-                return -log_prior.mean()/mean_pred.shape[-2]
+                return -log_prior.mean() / mean_pred.shape[-2]
 
             def neg_total_likelihood(predictions):
                 log_post = neg_log_post(predictions)
@@ -307,16 +385,16 @@ class KDEfWGDEnsemble(FSVGDEnsemble):
 
             log_post, log_posterior_grad = jax.vmap(value_and_grad(neg_total_likelihood, 0))(predictions)
             k_i = jnp.sum(k_pred, axis=1)
-            stein_grad = log_posterior_grad + jax.vmap(lambda x, y: x/y)(grad_k, k_i)
-            #stein_grad = (jnp.einsum('ij,jkm', k_pred, log_posterior_grad)
+            stein_grad = log_posterior_grad + jax.vmap(lambda x, y: x / y)(grad_k, k_i)
+            # stein_grad = (jnp.einsum('ij,jkm', k_pred, log_posterior_grad)
             #              + grad_k)
             grad = pred_vjp(stein_grad)[0]
             return log_post.mean(), grad
 
         loss, grads = kdeloss(params)
         updates, new_opt_state = update_fn(grads,
-                                                       opt_state,
-                                                       params=params)
+                                           opt_state,
+                                           params=params)
         new_params = optax.apply_updates(params, updates)
         grad_norm = optax.global_norm(grads)
         return new_params, new_opt_state, loss, grad_norm

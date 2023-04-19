237cec36a02ce2f881ed773723080ad582601be8
sukhijab <sukhijab@student.ethz.ch>
2023-04-19 22:39:04 +0200

    fixed cost issue for swimmer

--- mbse/models/environment_models/swimmer_reward.py
+++ mbse/models/environment_models/swimmer_reward.py
@@ -14,7 +14,7 @@ class SwimmerRewardModel(RewardModel):
     @partial(jax.jit, static_argnums=0)
     def predict(self, obs, action, next_obs=None, rng=None):
         nose_to_target = obs[..., -2:]
-        reward_control = - jnp.square(jnp.linalg.norm(action, axis=-1))
+        reward_control = - jnp.square(jnp.linalg.norm(action, axis=-1)) * self.ctrl_cost_weight
         reward = - jnp.square(jnp.linalg.norm(nose_to_target, axis=-1)) + reward_control
         reward = reward.reshape(-1).squeeze()
         return reward

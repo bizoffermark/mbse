118d9033775889017c20ec7e49e5d3e2d47df6b4
sukhijab <sukhijab@student.ethz.ch>
2023-04-20 09:51:32 +0200

    fixed swimmer reward

--- mbse/models/environment_models/swimmer_reward.py
+++ mbse/models/environment_models/swimmer_reward.py
@@ -7,14 +7,15 @@ from functools import partial
 class SwimmerRewardModel(RewardModel):
     """Get Pendulum Reward."""
 
-    def __init__(self, ctrl_cost_weight=0.01):
+    def __init__(self, ctrl_cost_weight=0.0):
         super().__init__()
         self.ctrl_cost_weight = ctrl_cost_weight
 
     @partial(jax.jit, static_argnums=0)
     def predict(self, obs, action, next_obs=None, rng=None):
         nose_to_target = obs[..., -2:]
-        reward_control = - jnp.square(jnp.linalg.norm(action, axis=-1)) * self.ctrl_cost_weight
-        reward = - jnp.square(jnp.linalg.norm(nose_to_target, axis=-1)) + reward_control
+        reward_dist = -jnp.linalg.norm(nose_to_target, axis=-1)
+        reward_ctrl = -jnp.square(action).sum(-1)
+        reward = reward_dist + self.ctrl_cost * reward_ctrl
         reward = reward.reshape(-1).squeeze()
         return reward
